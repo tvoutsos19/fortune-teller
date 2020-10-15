8ef487ebc2f4ff641d335a42b49911f1c6c1fb97
tvoutsos <tvoutsos@chicagobooth.edu>
2020-10-15 20:14:44 +0000

    Commit Over Half Way

--- config/routes.rb
+++ config/routes.rb
@@ -18,17 +18,17 @@ Rails.application.routes.draw do
   # Uncomment each route below ONE AT A TIME and debug.
   # Do NOT uncomment more than one at a time, or you'll be dealing with multiple syntax errors at once.
 
-  # get("/zodiacs/aries", { :controller =>  fire, action =>  "ram" })
-  # get("/zodiacs/leo", { :controller => "fire", :action => "lion" })
-  # get("/zodiacs/sagittarius" { :controller => "fire", :action => "archer" })
+  get("/zodiacs/aries", { :controller =>  "fires", :action =>  "ram" })
+  get("/zodiacs/leo", { :controller => "fires", :action => "lion" })
+  get("/zodiacs/sagittarius", { :controller => "fires", :action => "archer" })
   
-  # get("/zodiacs/taurus", { :controller, "earth", :action, "bull" })
-  # get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
-  # get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
+  get("/zodiacs/taurus", { :controller => "earth", :action => "bull" })
+  get("/zodiacs/virgo", { :controller => "earth", :action => "maiden" })
+  get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
   
-  # get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
-  # get(" /zodiacs/libra", { :controller => "air", :action => "scales" })
-  # get ("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
+  get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
+  get("/zodiacs/libra", { :controller => "air", :action => "scales" })
+  get ("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
   
   # get("/zodiacs/cancer " { :controller => "earth" :action => "crab" })
   # get("/zodiac/ scorpio", { :controller => "Water", :action => "scorpion" })
