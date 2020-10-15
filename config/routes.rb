3cb7eaaec539abe8be1e902055ad983f7ac9a593
tvoutsos <tvoutsos@chicagobooth.edu>
2020-10-15 20:30:13 +0000

    Final

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
   
   get("/zodiacs/cancer", { :controller => "waters", :action => "crab" })
   get("/zodiacs/scorpio", { :controller => "waters", :action => "scorpion" })
-  # get("/zodiacs/pisces", { :controller => "water", :action => "fish " })
+  get("/zodiacs/pisces", { :controller => "waters", :action => "fish" })
 
   # PART 3: MORE R→C→A→V PRACTICE
   # ==========================
